4e6803042b1f1df6f5f5e7321d2ff3c76fa146c7
George  Seletski <george.seletski@gmail.com>
2023-04-15 11:48:20 +0200

    feat: added thumbnails littli pngs

--- js/generating_contents.js
+++ js/generating_contents.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.sidebar-items');
 
-for (let i = 1; i < 132; i++) {
+for (let i = 1; i < 133; i++) {
     const listItem = document.createElement('li');
     const linkToPage = document.createElement('a');
     const spanTag = document.createElement('span');
