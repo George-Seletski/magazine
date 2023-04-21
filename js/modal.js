58107d5513f6883514456774ae9a3263fb295b96
George  Seletski <george.seletski@gmail.com>
2023-04-21 15:27:27 +0200

    feat: added social links

--- js/modal.js
+++ js/modal.js
@@ -2,6 +2,9 @@ const modal = document.getElementById('modal');
 const openModalBtn = document.getElementById('open-modal');
 //const closeModalBtn = document.getElementsByClassName('close')[0];
 
+const modalSocial = document.getElementById('modal-social-links');
+const openSocialModalBtn = document.getElementById('open-modal-social-links');
+
 
 // Open the modal
 openModalBtn.onclick = function() {
@@ -36,6 +39,43 @@ const element = document.querySelector('#thumbnails-items');
 element.addEventListener('click', function() {
     // hide the element
     modal.style.display = "none";
+
+
+});
+
+/*Social Modal Links Modal*/
+
+
+// Open the modal
+openSocialModalBtn.onclick = function() {
+    modalSocial.style.display = "block";
+    //$('#canvas').css('position', 'fixed');
+    $('.modal-social-links').css('position', 'none');
+}
+
+
+//Close the Modal from footer
+function closeModalSocials() {
+    modalSocial.style.display = "none";
+}
+
+
+
+
+// Close the modal if user clicks outside of it
+window.onclick = function(event) {
+    if (event.target == modalSocial) {
+        modalSocial.style.display = "none";
+    }
+    //$('.magazine-viewport').show();
+}
+
+
+const Socialelement = document.querySelector('#socila-items');
+
+element.addEventListener('click', function() {
+    // hide the element
+    modalSocial.style.display = "none";
 
 
 });
